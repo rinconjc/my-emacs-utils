bc9ccce67a78aeda31e7f3d7f8338095b848d1a6
Julio <julio@Julios-MBP.lan>
2023-07-22 10:32:29 +1000

    Add after save hook for auto exporting svg

--- my-utils.el
+++ my-utils.el
@@ -1,10 +1,15 @@
-(message "loading my package ...")
 (defun my-export-puml ()
   "Exports the current puml file to and svg file in the same directory"
   (interactive)
-  (when-let (file buffer-file-name)
+  (when (eq major-mode 'plantuml-mode)
+    (when-let (file buffer-file-name)
       (when (or (string-suffix-p ".pum" file) (string-suffix-p ".puml" file))
         (let ((cmd-args (append (list plantuml-java-command nil nil nil)
                                 (plantuml-jar-render-command "-tsvg" file))))
           (apply 'call-process cmd-args))
-        (message "puml exported"))))
+        (message "puml exported")))))
+
+(defun on-puml-save-hook ()
+  (add-hook 'after-save-hook 'my-export-puml nil t))
+
+(add-hook 'plantuml-mode-hook 'on-puml-save-hook)
